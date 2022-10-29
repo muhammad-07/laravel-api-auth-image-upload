3dbc0940a855e6f0b8de4259a4d380814e7d693c
veny21 <veny32402@gmail.com>
2022-10-29 18:07:17 +0530

    first commit

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Image;
+use Symfony\Component\HttpFoundation\Response;
+
+class ImageController extends Controller
+{
+    public function imageStore(Request $request)
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+        ]);
+
+        $image_path = $request->file('image')->store('images', 'public');
+        $oname = $request->file('image')->getClientOriginalName();
+        $data = Image::create([
+            'name' => $oname,
+            'path' => $image_path,
+        ]);
+
+        return response($data, Response::HTTP_CREATED);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('upload-image', [ImageController::class, 'imageStore'])->name('upload-image');
+});
+
+Route::controller(AuthController::class)->group(function(){
+
+    Route::post('test', 'test');
+    Route::post('register', 'register');
+
+    Route::post('login', 'login');
+
+});
